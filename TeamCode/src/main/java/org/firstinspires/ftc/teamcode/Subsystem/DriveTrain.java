98c10c32ececd8e458334c3b045d5c2efa515621
DoctorKrill08 <doctorkrill08@gmail.com>
2024-12-20 17:32:08 -0500

    Setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/DriveTrain.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class DriveTrain {
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+
+    public void initiate(HardwareMap hardwareMap) {
+        frontLeftMotor = hardwareMap.dcMotor.get("motor3");
+        frontRightMotor = hardwareMap.dcMotor.get("motor2");
+        backLeftMotor = hardwareMap.dcMotor.get("motor1");
+        backRightMotor = hardwareMap.dcMotor.get("motor0");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+}
